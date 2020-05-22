da28d1c275df35342fb86555a3323b24d819332d
KaalaAadmi <51954157+KaalaAadmi@users.noreply.github.com>
2020-05-23 02:00:55 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='binomial_gaussian',
+      version='0.1',
+      description='Binomial and Gaussian distributions',
+      packages=['binomial_gaussian'],
+      author='KaalaAadmi'
+      zip_safe=False)
